6bda35df098e4980d3820ac2831c22a9f8297123
Sergii Sukhanov <ssuhanov@gmail.com>
2026-07-19 17:49:38 +0200

    Prepare basic setup for FastAPI application running

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-from openai import OpenAI
+from fastapi import FastAPI
 
-client = OpenAI(api_key="")
+app = FastAPI()
 
-print("Available models:")
-for model in client.models.list():
-    print(model.id)
 
-current_model = "gpt-5.5"
-prompt = "Tell me how can I become an AI Engineer"
-print("Prompt: " + prompt)
-response = client.chat.completions.create(
-    model=current_model,
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt}
-    ]
-)
-
-print(response.choices[0].message.content)
