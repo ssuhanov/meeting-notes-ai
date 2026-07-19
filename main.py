02c3390089c800a3690a18c3fe83f38b5e1ed827
Sergii Sukhanov <ssuhanov@gmail.com>
2026-07-19 18:13:11 +0200

    Implement basic endpoint

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+@app.get("/")
+def hello():
+    return {
+        "name": "Meeting Notes AI",
+        "version": "1.0",
+        "status": "running"
+    }
